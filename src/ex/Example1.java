fff5b9cfca716505b0e1f2240c1e4e180651c6e6
kwucslkm <kwucsani@naver.com>
2023-03-14 11:12:43 +0900

    ex2 second commit

--- src/ex/Example1.java
+++ src/ex/Example1.java
@@ -3,13 +3,10 @@ package ex;
 public class Example1 {
 
 	public static void main(String[] args) {
-		for (int i = 1 ; i<10 ; i++) {
-			for(int j = 2 ; j <10 ; j++) {
-				System.out.print(j+"*"+i+"="+(j*i)+"\t");
-			}
-			System.out.println();
-			
-		}
+		
+		Example2 example2 = new Example2();
+		example2.numbertable();
+		
 
 	}
 
---
+++ src/ex/Example2.java
@@ -0,0 +1,14 @@
+package ex;
+
+public class Example2 {
+	public void numbertable() {
+		for (int i = 1 ; i<10 ; i++) {
+			for(int j = 2 ; j <10 ; j++) {
+				System.out.print(j+"*"+i+"="+(j*i)+"\t");
+			}
+			System.out.println();
+			
+		}
+	}
+	
+}
